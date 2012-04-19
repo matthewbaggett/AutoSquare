f005a19a3c77ede25e9b4da943a95f0cc843c866
geusebio <geusebio@c29433ff-1b1d-4321-a7e5-4b4d3a6625c3>
2012-04-19 14:59:11 +0000

    git-svn-id: http://svn.turbocrms.com/code/projects/AutoSquare@743 c29433ff-1b1d-4321-a7e5-4b4d3a6625c3

--- application/controllers/MeController.php
+++ application/controllers/MeController.php
@@ -7,44 +7,35 @@ class MeController extends Zend_Controller_Action
 	}
 	
 	public function addlatitudeAction(){
-		$oauthOptions = array(
-		    'requestScheme'        => Zend_Oauth::REQUEST_SCHEME_HEADER,
-		    'version'              => '1.0',
-		    'consumerKey'          => '120853944602-j79u0cinskab6ile6gvoin71pc3b9d5i.apps.googleusercontent.com',
-		    'consumerSecret'       => 'Eme_vSHbukhyu-LNNrnvFgnZ',
-		    'signatureMethod'      => 'HMAC-SHA1',
-		    'requestTokenUrl'      => 'https://www.google.com/accounts/OAuthGetRequestToken',
-		    'userAuthorizationUrl' => 'https://www.google.com/latitude/apps/OAuthAuthorizeToken',
-		    'accessTokenUrl'       => 'https://www.google.com/accounts/OAuthGetAccessToken',
-		    'callbackUrl'          => 'http://autosquare.turbocrms.com:8080/Me/AddLatitude/?show=callback',
+		$CONSUMER_KEY = '120853944602-j79u0cinskab6ile6gvoin71pc3b9d5i.apps.googleusercontent.com';
+		$CONSUMER_SECRET = 'Eme_vSHbukhyu-LNNrnvFgnZ';
+		
+		// Multi-scoped token.
+		$SCOPES = array(
+		  'https://docs.google.com/feeds/',
+		  'https://spreadsheets.google.com/feeds/'
 		);
-		$consumer = new Zend_Oauth_Consumer($oauthOptions); 
-		if (!isset($_SESSION['ACCESS_TOKEN_GOOGLE'])) { 
-		    if (!empty($_GET)) { 
-		        $token = $consumer->getAccessToken($_GET, unserialize($_SESSION['REQUEST_TOKEN_GOOGLE'])); 
-		        $_SESSION['ACCESS_TOKEN_GOOGLE'] = serialize($token); 
-		    } else { 
-		        $token = $consumer->getRequestToken(array('scope'=>'https://www.googleapis.com/auth/latitude')); 
-		        $_SESSION['REQUEST_TOKEN_GOOGLE'] = serialize($token); 
-		        $customparams = array('domain' => 'autosquare.turbocrms.com', 'granularity' => 'best', 'location' => 'current');
-		        $consumer->redirect($customparams ); 
-		        
-		        exit; 
-		    } 
-		} else { 
-		    $token = unserialize($_SESSION['ACCESS_TOKEN_GOOGLE']); 
-		    //$_SESSION['ACCESS_TOKEN_GOOGLE'] = null; // do not use, we want to keep the access token
-		} 
-		$client = $token->getHttpClient($oauthOptions); 
-		$client->setUri('https://www.googleapis.com/latitude/v1/currentLocation'); 
-		$client->setMethod(Zend_Http_Client::GET); 
 		
+		$oauthOptions = array(
+		  'requestScheme' => Zend_Oauth::REQUEST_SCHEME_HEADER,
+		  'version' => '1.0',
+		  'consumerKey' => $CONSUMER_KEY,
+		  'consumerSecret' => $CONSUMER_SECRET,
+		  'signatureMethod' => 'HMAC-SHA1',
+		  'callbackUrl' => 'http://myapp.example.com/access_token.php',
+		  'requestTokenUrl' => 'https://www.google.com/accounts/OAuthGetRequestToken',
+		  'userAuthorizationUrl' => 'https://www.google.com/accounts/OAuthAuthorizeToken',
+		  'accessTokenUrl' => 'https://www.google.com/accounts/OAuthGetAccessToken'
+		);
 		
-		$response = $client->request(); 
-		$body = $response->getBody();
-		header('Content-Type: ' . $response->getHeader('Content-Type')); 
-		echo $response->getBody(); 
-			
+		$consumer = new Zend_Oauth_Consumer($oauthOptions);
+		
+		// When using HMAC-SHA1, you need to persist the request token in some way.
+		// This is because you'll need the request token's token secret when upgrading
+		// to an access token later on. The example below saves the token object as a session variable.
+		if (!isset($_SESSION['ACCESS_TOKEN'])) {
+		  $_SESSION['REQUEST_TOKEN'] = serialize($consumer->getRequestToken(array('scope' => implode(' ', $SCOPES))));
+		}
 	}
    
 }
