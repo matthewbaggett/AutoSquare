bf16f79049369061d3b3e58c444d83f0697837a7
geusebio <geusebio@c29433ff-1b1d-4321-a7e5-4b4d3a6625c3>
2012-04-20 11:24:51 +0000

    Token!
    git-svn-id: http://svn.turbocrms.com/code/projects/AutoSquare@763 c29433ff-1b1d-4321-a7e5-4b4d3a6625c3

--- application/controllers/MeController.php
+++ application/controllers/MeController.php
@@ -6,7 +6,8 @@ class MeController extends Zend_Controller_Action
 		
 	}
 	public function showsessionAction(){
-		print_r($_SESSION);
+		$google_latitude_access_token = Turbo_Model_User::getCurrentUser()->settingGet("google_latitude_access_token");
+		$this->view->assign("google_latitude_access_token",$google_latitude_access_token);
 	}
 	
 	public function addlatitudeAction(){
