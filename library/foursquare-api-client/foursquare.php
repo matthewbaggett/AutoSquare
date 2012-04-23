92512a8812f01362d70a8e7016a70aaea12c4176
geusebio <geusebio@c29433ff-1b1d-4321-a7e5-4b4d3a6625c3>
2012-04-23 11:13:19 +0000

    git-svn-id: http://svn.turbocrms.com/code/projects/AutoSquare@801 c29433ff-1b1d-4321-a7e5-4b4d3a6625c3

--- library/foursquare-api-client/foursquare.php
+++ library/foursquare-api-client/foursquare.php
@@ -3,7 +3,7 @@
 class foursquare_api{
 	private $client_id = 'A0E0UFLSVUV45SMMCD0O0J4OIO0I3OFJJB5U3SZ0IFSVNYJC';
 	private $client_secret = '2UZWRFA2WL2UK4N1LCF3SBLPLNF0IBMI4BGD04S1QWC0VNTA';
-	private $redirect_uri = 'http://autosquare.turbocrms.com/Me/add-foursquare-callback';
+	private $redirect_uri = 'http://autosquare.turbocrms.com/Foursquare/add-foursquare-callback';
 	
 	public function redirect_get_access_token(){
 		$redirect_url = "https://foursquare.com/oauth2/authenticate?client_id={$this->client_id}&response_type=code&redirect_uri={$this->redirect_uri}";
