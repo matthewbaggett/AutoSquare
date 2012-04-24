178a6edf8ed25bf65a4e17fcac760de4c694ad73
geusebio <geusebio@c29433ff-1b1d-4321-a7e5-4b4d3a6625c3>
2012-04-24 15:45:17 +0000

    git-svn-id: http://svn.turbocrms.com/code/projects/AutoSquare@826 c29433ff-1b1d-4321-a7e5-4b4d3a6625c3

--- application/controllers/GoogleController.php
+++ application/controllers/GoogleController.php
@@ -40,7 +40,7 @@ class GoogleController extends Turbo_Controller_LoggedInAction
 	
 	private function _get_latitude_locations($count = 100){
 		list($service, $client) = $this->_set_up_google_api();
-		$location = $service->location->listLocation(array('granularity' => 'best'));
+		$location = $service->location->listLocation(array('granularity' => 'best','max-results' => 1000));
 		return $location['items'];
 	}
 	
