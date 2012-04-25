2da95e8e95f73ae66d26b7b1bfc3a8095e61af72
geusebio <geusebio@c29433ff-1b1d-4321-a7e5-4b4d3a6625c3>
2012-04-25 14:12:57 +0000

    git-svn-id: http://svn.turbocrms.com/code/projects/AutoSquare@831 c29433ff-1b1d-4321-a7e5-4b4d3a6625c3

--- application/controllers/GameController.php
+++ application/controllers/GameController.php
@@ -1,6 +1,6 @@
 <?php 
 class GameController extends Turbo_Controller_LoggedInAction{
-	public function CheckForAchievementsAction(){
+	public function checkForAchievementsAction(){
 		$game_instance = new Game_Core(Turbo_Model_User::getCurrentUser());
 		$arr_new_achievements = $game_instance->check_for_achievements();
 		
