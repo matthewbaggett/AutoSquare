1f27ad0191e2ae04ff82a109d435c0c64eebbad4
geusebio <geusebio@c29433ff-1b1d-4321-a7e5-4b4d3a6625c3>
2012-04-25 16:28:25 +0000

    git-svn-id: http://svn.turbocrms.com/code/projects/AutoSquare@835 c29433ff-1b1d-4321-a7e5-4b4d3a6625c3

--- library/Game/Core.php
+++ library/Game/Core.php
@@ -7,6 +7,10 @@ class Game_Core{
 	}
 	
 	public function check_for_achievements(){
-		
+		foreach($this->user->getUncheckedLocations() as $user_location){
+			$achievement_locations_search = new Game_Model_DbTable_AchievementLocationsSearch();
+			$arr_achievements_to_award = $achievement_locations_search->do_lookup($user_location->posLatitude, $user_location->posLongitude);
+			$this->user->award($arr_achievements_to_award);
+		}
 	}
 }
---
+++ library/Game/Model/DbTable/AchievementLocationsSearch.php
@@ -0,0 +1,10 @@
+<?php
+
+class Game_Model_DbTable_AchievementLocations extends Zend_Db_Table_Abstract
+{
+
+    protected $_name = 'tblAchievementLocations';
+
+    
+}
+
