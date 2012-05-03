1ede8acda69a57ccfc020b33d440767968b48eb4
geusebio <geusebio@c29433ff-1b1d-4321-a7e5-4b4d3a6625c3>
2012-05-03 13:18:49 +0000

    git-svn-id: http://svn.turbocrms.com/code/projects/AutoSquare@882 c29433ff-1b1d-4321-a7e5-4b4d3a6625c3

--- application/controllers/CronController.php
+++ application/controllers/CronController.php
@@ -5,7 +5,9 @@ class CronController extends Zend_Controller_Action
 	public function updateLatitudeFeedsAction(){
 		$tblUsers = new Turbo_Model_DbTable_Users();
 		$arr_users = $tblUsers->fetchAll();
-		print_r($arr_users);
+		foreach($arr_users as $user){
+			echo "{$user->strUsername}\n";
+		}
 		exit;
 	}
 }
