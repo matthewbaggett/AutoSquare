96465b18c276e5baced5855c015294f566b696eb
geusebio <geusebio@c29433ff-1b1d-4321-a7e5-4b4d3a6625c3>
2012-05-09 16:01:37 +0000

    git-svn-id: http://svn.turbocrms.com/code/projects/AutoSquare@954 c29433ff-1b1d-4321-a7e5-4b4d3a6625c3

--- library/Game/Core.php
+++ library/Game/Core.php
@@ -4,7 +4,17 @@ class Game_Core{
 	
 	public function __construct(Application_Model_User $user){
 		$this->user = $user;
-		
+	}
+	
+	protected function _refine_distance_search($arr_rough_search){
+		$arr_locations_in_radius = array();
+		foreach($arr_rough_search as $potential_location){
+			$distance = $this->distance_haversine($latitude, $longitude, $potential_location->locLatitude, $potential_location->locLongitude);
+			if($distance <= $potential_location->intRadius){
+				$arr_locations_in_radius[$potential_location->strName] = $potential_location;
+			}
+		}
+		return $arr_locations_in_radius;
 	}
 	
 	protected function _check_for_achievements_for_userlocation($user_location){
@@ -17,13 +27,9 @@ class Game_Core{
 		$select->where('locLongitude - (intRadius/(85*1000)) < ?', 	$user_location->locLongitude);
 		$select->where('locLongitude + (intRadius/(85*1000)) > ?', 	$user_location->locLongitude);
 		$arr_rough_search = $tblAchievementLocations->fetchAll($select);
+		
 		// loop over these rough results, and compute the distances:
-		foreach($arr_rough_search as $potential_location){
-			$distance = $this->distance_haversine($latitude, $longitude, $potential_location->locLatitude, $potential_location->locLongitude);
-			if($distance <= $potential_location->intRadius){
-				$arr_locations_in_radius[$potential_location->strName] = $potential_location;
-			}
-		}
+		$arr_locations_in_radius = $this->_refine_distance_search($arr_rough_search);
 			
 		return $arr_locations_in_radius;
 	}
