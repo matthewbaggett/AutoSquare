b59c38a9c23b3221c4e9665d522fe607dbe51d7e
geusebio <geusebio@c29433ff-1b1d-4321-a7e5-4b4d3a6625c3>
2012-05-09 15:55:36 +0000

    git-svn-id: http://svn.turbocrms.com/code/projects/AutoSquare@951 c29433ff-1b1d-4321-a7e5-4b4d3a6625c3

--- library/Game/Model/User.php
+++ library/Game/Model/User.php
@@ -1,6 +1,6 @@
 <?php 
 class Game_Model_User extends Turbo_Model_User{
-	public function award($award){
+	public function award_location($award){
 		if(is_array($award)){
 			foreach($award as $iaward){
 				$this->award($iaward);
