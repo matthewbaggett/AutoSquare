7b0a10da8fbe0b8895b172ae06396c500ae2ca1e
geusebio <geusebio@c29433ff-1b1d-4321-a7e5-4b4d3a6625c3>
2012-05-09 14:46:32 +0000

    git-svn-id: http://svn.turbocrms.com/code/projects/AutoSquare@929 c29433ff-1b1d-4321-a7e5-4b4d3a6625c3

--- application/controllers/MeController.php
+++ application/controllers/MeController.php
@@ -21,6 +21,15 @@ class MeController extends Turbo_Controller_LoggedInAction
 		$this->view->headScript()->appendFile("http://maps.googleapis.com/maps/api/js?key=AIzaSyAeDI_T5MhRJtykibKEqszGZAxxGB3iaTg&sensor=true");
 		$this->view->headScript()->appendFile($this->view->baseUrl() . "/js/application-me-map.js");
 		
+		$tblUserLocations = new Game_Model_DbTable_UserLocations();
+		
+		$sel = $tblUserLocations->select();
+		$sel->where('intUserId = ?', Turbo_Model_User::getCurrentUser()->intUserId);
+		$sel->order('dtmTimestamp DESC');
+		$sel->limit(100);
+		
+		$this->view->assign("arr_locations",$tblUserLocations->fetchAll($sel));
+		
 	}
 }
 
