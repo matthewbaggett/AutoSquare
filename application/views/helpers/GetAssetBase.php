f3bc542c87d42f8aa12789f1cbc7015102e7217c
geusebio <geusebio@c29433ff-1b1d-4321-a7e5-4b4d3a6625c3>
2012-05-09 20:27:55 +0000

    git-svn-id: http://svn.turbocrms.com/code/projects/AutoSquare@971 c29433ff-1b1d-4321-a7e5-4b4d3a6625c3

--- application/views/helpers/GetAssetBase.php
+++ application/views/helpers/GetAssetBase.php
@@ -5,22 +5,12 @@ class Application_View_Helper_GetAssetBase extends Zend_View_Helper_Abstract
     {
     	$protocol = $_SERVER['SERVER_PORT'] == 443 ? 'https://':'http://';
     	
-    	if($_SERVER['HTTP_HOST'] == 'beta.bunnehbutt.com'){
-    		$arr_servers = array(
-    				"beta.bunnehbutt.com"
-    		);
-    	}else{
-    		$arr_servers = array(
-    				"renamon.img.bunnehbutt.com",
-    				"terriermon.img.bunnehbutt.com",
-    				"guilmon.img.bunnehbutt.com",
-    		);
-    	}
+    	
     	
     	if($protocol != "https://"){
         	$server = $arr_servers[rand(0,count($arr_servers)-1)];
     	}else{
-    		$server = "ssl.bunnehbutt.com";
+    		$server = "ssl.fff.com";
     	}
     	return $protocol.$server;
         
