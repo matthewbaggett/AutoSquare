358ecdd19420e839e0f2bc7039452027b66f54a9
geusebio <geusebio@c29433ff-1b1d-4321-a7e5-4b4d3a6625c3>
2012-05-10 07:44:02 +0000

    git-svn-id: http://svn.turbocrms.com/code/projects/AutoSquare@988 c29433ff-1b1d-4321-a7e5-4b4d3a6625c3

---
+++ library/Game/Doc/script.wipe_generated_speeds.sql
@@ -0,0 +1,11 @@
+UPDATE tblUserLocations
+SET intPrevUserLocationID = null, 
+locPrevLatitude = null, 
+locPrevLongitude = null,
+numSpeed = null,
+numPrevSpeed = null
+numDistance = null, 
+numPrevBearing = null,
+intTimeSinceLastLocationMs = null
+
+WHERE 1=1
