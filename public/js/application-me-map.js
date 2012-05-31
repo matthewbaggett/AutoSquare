1007aeeca66d81043fbbed044fb93193d964528b
geusebio <geusebio@c29433ff-1b1d-4321-a7e5-4b4d3a6625c3>
2012-05-31 15:08:18 +0000

    git-svn-id: http://svn.turbocrms.com/code/projects/AutoSquare@1052 c29433ff-1b1d-4321-a7e5-4b4d3a6625c3

--- public/js/application-me-map.js
+++ public/js/application-me-map.js
@@ -69,21 +69,9 @@ function initialize_overlay(){
 	
 }
 $(document).ready(function(){
-	var dates = $( "#from, #to" ).datepicker({
-		defaultDate: "+1w",
-		changeMonth: true,
-		numberOfMonths: 3,
-		onSelect: function( selectedDate ) {
-			var option = this.id == "from" ? "minDate" : "maxDate",
-				instance = $( this ).data( "datepicker" ),
-				date = $.datepicker.parseDate(
-					instance.settings.dateFormat ||
-					$.datepicker._defaults.dateFormat,
-					selectedDate, instance.settings );
-			dates.not( this ).datepicker( "option", option, date );
-		}
-	});
-	dates.datepicker( "option", "dateFormat","YYYY-MM-DD");
+	$( "#from, #to" )
+		.datepicker()
+		.datepicker( "option", "dateFormat","yy/mm/dd");
 
 	initialize_map();
 });
