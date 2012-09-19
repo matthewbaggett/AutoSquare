672f4e9928324a850a0e66552bf9b22a21ae9a80
geusebio <geusebio@c29433ff-1b1d-4321-a7e5-4b4d3a6625c3>
2012-09-19 22:17:33 +0000

    git-svn-id: http://svn.turbocrms.com/code/projects/AutoSquare@1704 c29433ff-1b1d-4321-a7e5-4b4d3a6625c3

--- application/controllers/FoursquareController.php
+++ application/controllers/FoursquareController.php
@@ -1,8 +1,18 @@
 <?php
+require_once(APPLICATION_PATH . "/../library/Eden/eden.php");
+require_once(APPLICATION_PATH . "/../library/Eden/eden/foursquare.php");
 
 class FoursquareController extends Turbo_Controller_LoggedInAction
 {
+	private $client_id = 'A0E0UFLSVUV45SMMCD0O0J4OIO0I3OFJJB5U3SZ0IFSVNYJC';
+	private $client_secret = '2UZWRFA2WL2UK4N1LCF3SBLPLNF0IBMI4BGD04S1QWC0VNTA';
+	private $redirect_uri = 'https://gamitu.de/Foursquare/add-foursquare-callback';
+	private $auth;
 
+	public function init(){
+		parent::init();
+		$this->auth = eden('foursquare')->auth($this->client_id, $this->client_secret, $this->redirect_uri);
+	}
 	private function _include_foursquare_api(){
 		require_once dirname(__FILE__) . "/../../library/simple-api-clients/foursquare.php";
 	}
@@ -13,16 +23,16 @@ class FoursquareController extends Turbo_Controller_LoggedInAction
 	}
 	
 	public function addFoursquareAction(){
-		$this->_include_foursquare_api();
-		$fsq = new foursquare_api();
-		$fsq->redirect_get_access_token();
+		$login = $this->auth->getLoginUrl();
+		header("Location: $login");
+		exit;
 	}
 	
 	public function addFoursquareCallbackAction(){
-		$this->_include_foursquare_api();
-		$fsq = new foursquare_api();
-		$access_token = $fsq->get_access_token($_GET['code']);
-		Application_Model_User::getCurrentUser()->settingSet('foursquare_access_token',$access_token);
+		$access = $this->auth->getAccess($_GET['code']);
+		
+		Application_Model_User::getCurrentUser()->settingSet('foursquare_access_token',$access['access token']);
+		
 		$this->_helper->redirector('add-foursquare-complete');
 	}
 
